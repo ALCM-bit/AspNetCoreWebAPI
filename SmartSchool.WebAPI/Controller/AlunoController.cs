ba8ba79820be09f5548789ab58a7321871bb3eb5
Felipe ALCM <felipealcmagalhaes@gmail.com>
2023-03-13 18:25:34 -0300

    Adding first part of the project

---
+++ SmartSchool.WebAPI/Controller/AlunoController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.WebAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>() {
+            new Aluno() { Id = 1, Nome = "Felipe", Sobrenome = "Augusto", Telefone = "123456"},
+            new Aluno() { Id = 2, Nome = "Marta", Sobrenome = "Kent", Telefone = "123456"},
+            new Aluno() { Id = 3, Nome = "Maria",Sobrenome = "Dolores", Telefone = "123456"}
+        };
+
+        public AlunoController() { }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        //http://localhost:5015/api/aluno/byId/1 faz parte da rota
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+
+            return Ok(aluno);
+        }
+
+        //http://localhost:5015/api/aluno/byName?name=Marta Usando queryString
+        //http://localhost:5015/api/aluno/byName?nome=Felipe&sobrenome=Augusto
+        [HttpGet("byName")]
+        public IActionResult GetByName(string nome, string sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome) && a.Sobrenome.Contains(sobrenome));
+            if (aluno == null) return BadRequest("Aluno não encontrado");
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SmartSchool.WebAPI/Controller/ProfessorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.WebAPI.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        public List<Professor> Professores = new List<Professor>() {
+            new Professor() { Id = 1, Nome = "Marcos"},
+            new Professor() { Id = 2, Nome = "Jose"},
+            new Professor() {Id = 3, Nome = "Mario"}
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Professores);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var professor = Professores.FirstOrDefault(p => p.Id == id);
+            if (professor == null) return BadRequest("Professor não encontrado");
+            return Ok(professor);
+        }
+    }
+}
